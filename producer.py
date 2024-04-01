0b2aa7d725bb36bc4c1c930958ccdd2d90f2fb9c
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2024-04-01 04:42:16 -0400

    add ride record defs and resources. modify producer config

--- producer.py
+++ producer.py
@@ -1,43 +1,116 @@
+import os
 import csv
-import json
-from typing import List, Dict
-from kafka import KafkaProducer
-from kafka.errors import KafkaTimeoutError
+from time import sleep
+from typing import Dict
+import socket
 
-from ride import Ride
-from settings import BOOTSTRAP_SERVERS, INPUT_DATA_PATH, KAFKA_TOPIC
+from confluent_kafka import Producer
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.serialization import SerializationContext, MessageField
+
+from ride_record_key import RideRecordKey, ride_record_key_to_dict
+from ride_record import RideRecord, ride_record_to_dict
+
+# from settings import RIDE_KEY_SCHEMA_PATH, RIDE_VALUE_SCHEMA_PATH, \
+#     SCHEMA_REGISTRY_URL, BOOTSTRAP_SERVERS, INPUT_DATA_PATH, KAFKA_TOPIC
+
+from dotenv import load_dotenv
+load_dotenv()
+
+INPUT_DATA_PATH = os.environ["INPUT_DATA_PATH"]
+RIDE_KEY_SCHEMA_PATH = os.environ["RIDE_KEY_SCHEMA_PATH"]
+RIDE_VALUE_SCHEMA_PATH = os.environ["RIDE_VALUE_SCHEMA_PATH"]
+SCHEMA_REGISTRY_URL = os.environ["SCHEMA_REGISTRY_URL"]
+BOOTSTRAP_SERVERS = os.environ["BOOTSTRAP_SERVERS"]
+CLUSTER_API_KEY = os.environ["CLUSTER_API_KEY"]
+CLUSTER_API_SECRET = os.environ["CLUSTER_API_SECRET"]
+KAFKA_TOPIC = os.environ["KAFKA_TOPIC"]
 
 
-class JsonProducer(KafkaProducer):
+def delivery_report(err, msg):
+    if err is not None:
+        print("Delivery failed for record {}: {}".format(msg.key(), err))
+        return
+    print('Record {} successfully produced to {} [{}] at offset {}'.format(
+        msg.key(), msg.topic(), msg.partition(), msg.offset()))
+
+
+class RideAvroProducer:
     def __init__(self, props: Dict):
-        self.producer = KafkaProducer(**props)
+        # Schema Registry and Serializer-Deserializer Configurations
+        key_schema_str = self.load_schema(props['schema.key'])
+        value_schema_str = self.load_schema(props['schema.value'])
+        schema_registry_props = {'url': props['schema_registry.url']}
+        schema_registry_client = SchemaRegistryClient(schema_registry_props)
+        self.key_serializer = AvroSerializer(schema_registry_client, key_schema_str, ride_record_key_to_dict)
+        self.value_serializer = AvroSerializer(schema_registry_client, value_schema_str, ride_record_to_dict)
+
+        # Producer Configuration
+        producer_props = props['producer_props']
+        self.producer = Producer(producer_props)
+
+    @staticmethod
+    def load_schema(schema_path: str):
+        path = os.path.realpath(os.path.dirname(__file__))
+        with open(f"{path}/{schema_path}") as f:
+            schema_str = f.read()
+        return schema_str
+
+    @staticmethod
+    def delivery_report(err, msg):
+        if err is not None:
+            print("Delivery failed for record {}: {}".format(msg.key(), err))
+            return
+        print('Record {} successfully produced to {} [{}] at offset {}'.format(
+            msg.key(), msg.topic(), msg.partition(), msg.offset()))
 
     @staticmethod
     def read_records(resource_path: str):
-        records = []
+        ride_records, ride_keys = [], []
         with open(resource_path, 'r') as f:
             reader = csv.reader(f)
-            header = next(reader)  # skip the header row
+            header = next(reader)  # skip the header
             for row in reader:
-                records.append(Ride(arr=row))
-        return records
+                ride_records.append(RideRecord(arr=[row[0], row[3], row[4], row[9], row[16]]))
+                ride_keys.append(RideRecordKey(vendor_id=int(row[0])))
+        return zip(ride_keys, ride_records)
 
-    def publish_rides(self, topic: str, messages: List[Ride]):
-        for ride in messages:
+    def publish(self, topic: str, records: [RideRecordKey, RideRecord]):
+        for key_value in records:
+            key, value = key_value
             try:
-                record = self.producer.send(topic=topic, key=ride.pu_location_id, value=ride)
-                print('Record {} successfully produced at offset {}'.format(ride.pu_location_id, record.get().offset))
-            except KafkaTimeoutError as e:
-                print(e.__str__())
+                self.producer.produce(topic=topic,
+                                      key=self.key_serializer(key, SerializationContext(topic=topic,
+                                                                                        field=MessageField.KEY)),
+                                      value=self.value_serializer(value, SerializationContext(topic=topic,
+                                                                                              field=MessageField.VALUE)),
+                                      on_delivery=delivery_report)
+            except KeyboardInterrupt:
+                break
+            except Exception as e:
+                print(f"Exception while producing record - {value}: {e}")
+
+        self.producer.flush()
+        sleep(1)
 
 
-if __name__ == '__main__':
-    # Config Should match with the KafkaProducer expectation
+if __name__ == "__main__":
     config = {
-        'bootstrap_servers': BOOTSTRAP_SERVERS,
-        'key_serializer': lambda key: str(key).encode(),
-        'value_serializer': lambda x: json.dumps(x.__dict__, default=str).encode('utf-8')
+        'schema_registry.url': SCHEMA_REGISTRY_URL,
+        'schema.key': RIDE_KEY_SCHEMA_PATH,
+        'schema.value': RIDE_VALUE_SCHEMA_PATH,
+        'producer_props': {
+            'bootstrap.servers': 'pkc-abcd85.us-west-2.aws.confluent.cloud:9092',
+            'security.protocol': 'SASL_SSL',
+            'sasl.mechanism': 'PLAIN',
+            'sasl.username': '<CLUSTER_API_KEY>',
+            'sasl.password': '<CLUSTER_API_SECRET>',
+            'client.id': socket.gethostname()
+        }
     }
-    producer = JsonProducer(props=config)
-    rides = producer.read_records(resource_path=INPUT_DATA_PATH)
-    producer.publish_rides(topic=KAFKA_TOPIC, messages=rides)
+
+    producer = RideAvroProducer(props=config)
+    ride_records = producer.read_records(resource_path=INPUT_DATA_PATH)
+    producer.publish(topic=KAFKA_TOPIC, records=ride_records)
+
