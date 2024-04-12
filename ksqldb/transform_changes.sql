afde3b6ab102600de3fc0254c846c542bd2a6687
josephj1o4e1 <josephj1o4e1.goofygroove@gmail.com>
2024-04-12 06:40:45 -0400

    ksqldb transformation: explode+unnest

---
+++ ksqldb/transform_changes.sql
@@ -0,0 +1,27 @@
+-- On the WebUI of ksqldb, import coinbase_avro topic as a stream
+-- Firstly, Explode nested array "changes" to multiple rows. 
+CREATE STREAM coinbase_avro_explode AS
+SELECT
+    type,
+    product_id,
+    EXPLODE(changes) AS change,
+    time
+FROM
+    coinbase_avro
+EMIT CHANGES
+;
+
+
+-- Secondly, Unnest coinbase_avro_explode "change" to multiple columns. change[0] should be metadata, skip it. 
+CREATE STREAM coinbase_avro_flat AS
+SELECT
+    type,
+    product_id,
+    change[1] AS change_side,
+    CAST(change[2] AS DOUBLE) AS change_size,
+    CAST(change[3] AS DOUBLE) AS change_price,
+    time
+FROM
+    coinbase_avro_explode
+EMIT CHANGES
+;
